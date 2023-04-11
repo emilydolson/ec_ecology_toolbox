8fb1a98d6915f9f151025e66fd92989122ecdcfe
Emily Dolson <emilyldolson@gmail.com>
2023-04-11 15:45:14 -0400

    Add module directory

---
+++ eco_ec_toolbox/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for eco_ec_toolbox."""
+
+__author__ = """Emily Dolson"""
+__email__ = 'emilydolson@gmail.com'
+__version__ = '0.1.13'
+
+from eco_ec_toolbox import *
+
+__all__ = [
+    "eco_ec_toolbox",
+]
