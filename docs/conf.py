8d7f87809e6bc7fe5189aa2e7e1736bd3796a393
Emily Dolson <emilyldolson@gmail.com>
2023-05-01 14:18:33 -0400

    Bump version: 0.0.3 → 0.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ copyright = '2023, Emily Dolson'
 author = 'Emily Dolson'
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.3"
+release = "0.0.4"
 
 
 # -- General configuration ---------------------------------------------------
