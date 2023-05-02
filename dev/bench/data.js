586ef03ea9b05515daa4ffa9e54ebf4a028a6846
github-action-benchmark <github@users.noreply.github.com>
2023-05-02 01:40:20 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for c391d2dc131fe1385fa65347608d53ad0c9e35b9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1682991613787,
+  "lastUpdate": 1682991620479,
   "repoUrl": "https://github.com/emilydolson/ec_ecology_toolbox",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -341,6 +341,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000015195776853694526",
             "extra": "mean: 171.5025705294037 usec\nrounds: 3814"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "emilyldolson@gmail.com",
+            "name": "Emily Dolson",
+            "username": "emilydolson"
+          },
+          "committer": {
+            "email": "emilyldolson@gmail.com",
+            "name": "Emily Dolson",
+            "username": "emilydolson"
+          },
+          "distinct": true,
+          "id": "c391d2dc131fe1385fa65347608d53ad0c9e35b9",
+          "message": "Fix permissions",
+          "timestamp": "2023-05-01T21:38:55-04:00",
+          "tree_id": "4d522221267fdb9e4190aa51552400c337343728",
+          "url": "https://github.com/emilydolson/ec_ecology_toolbox/commit/c391d2dc131fe1385fa65347608d53ad0c9e35b9"
+        },
+        "date": 1682991606053,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_toolbox.py::test_benchmark_lex_prob",
+            "value": 5102.407617718339,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007157694324366251",
+            "extra": "mean: 195.9859099707078 usec\nrounds: 3410"
           }
         ]
       }
